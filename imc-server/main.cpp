8671f63845dece7d1a1cf7460aae31b4cb49e3e7
Noah Huppert <noahhuppert@gmail.com>
2015-09-01 23:16:07 -0400

    Wrote simple stream socket code, got rid of libsocket

--- imc-server/main.cpp
+++ imc-server/main.cpp
@@ -1,15 +1,15 @@
-//#include <libsocket/inetserverstream.hpp>
-//#include <libsocket/exception.hpp>
-
 #include <iostream>
 #include <thread>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 
 /* Socket */
 int socket_fd = -1;
-sockaddr_in socket_address, client_address;
+int socket_port = 1234;
+
+struct sockaddr_in socket_address, client_address;
 const int socket_connection_backlog_size = 10;
 
 void start_server() {
@@ -23,20 +23,28 @@ void start_server() {
         std::cout << "Created socket(fd => " << socket_fd << ")" << std::endl;
     }
 
+    // Tell socket to reuse address
+    int resuse_addr = 1;
+    int set_reuse_addr_result = setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, (char *) &resuse_addr, sizeof(resuse_addr));
+
+    if (set_reuse_addr_result < 0) {
+        std::cerr << "Failed to set \"SO_REUSEADDR\"(fd => " << socket_fd << ")" << std::endl;
+    }
+
     // Bind socket
     bzero((char *) &socket_address, sizeof(socket_address));
 
     socket_address.sin_family = AF_INET;
     socket_address.sin_addr.s_addr = INADDR_ANY;
-    socket_address.sin_port = htons(1234);
+    socket_address.sin_port = htons(socket_port);
 
     int bind_result = bind(socket_fd, (struct sockaddr *) &socket_address, sizeof(socket_address));
 
     if (bind_result < 0) {
-        std::cerr << "Error binding socket" << std::endl;
+        std::cerr << "Error binding socket(errno => " << strerror(errno) << ")" << std::endl;
         exit(EXIT_FAILURE);
     } else {
-        std::cout << "Bound socket(port => " << socket_address.sin_port << ")" << std::endl;
+        std::cout << "Bound socket(port => " << socket_port << ")" << std::endl;
     }
 
     // Listen on address
@@ -50,7 +58,14 @@ void start_server() {
     }
 }
 
+void stop_server() {
+    close(socket_fd);
+
+    std::cout << "Closed socket(fd => " << socket_fd << ")" << std::endl;
+}
+
 void socket_accept() {
+    // Accept connection
     int client_len;
     int client_socket_fd = accept(socket_fd, (struct sockaddr *) &client_address, (socklen_t * ) &client_len);
 
@@ -58,15 +73,30 @@ void socket_accept() {
         std::cerr << "Failed to accept client connection" << std::endl;
         exit(EXIT_FAILURE);
     } else {
-        std::cout << "Accepted socket connection(address => " << client_address.sin_addr.s_addr
-                                                              << ", port => " << client_address.sin_port
-                                                << ")" << std::endl;
+        std::cout << "Accepted client socket connection(fd => " << client_socket_fd << ")" << std::endl;
     }
 
+    // Receive content
     const int receive_buffer_size = 256;
     char receive_buffer[receive_buffer_size];
 
-    recv(client_socket_fd, receive_buffer, receive_buffer_size, 0);
+    bzero(receive_buffer, receive_buffer_size - 1);
+
+    int receive_result = recv(client_socket_fd, receive_buffer, receive_buffer_size - 1, 0);
+
+    if (receive_result < 0) {
+        std::cerr << "Failed to receive from client socket(fd => " << client_socket_fd << ")" << std::endl;
+    } else {
+        std::cout << "Received from client socket(fd => " << client_socket_fd << std::endl
+                  << "                     content => " << receive_buffer << ")" << std::endl;
+    }
+
+    // Send response
+    send(client_socket_fd, receive_buffer, receive_buffer_size - 1, 0);
+
+    close(client_socket_fd);
+
+    std::cout << "Closed client socket connection(fd => " << client_socket_fd << ")" << std::endl;
 }
 
 int main() {
@@ -74,6 +104,8 @@ int main() {
 
     std::thread socket_accept_thread(socket_accept);
     socket_accept_thread.join();
+
+    stop_server();
 
     return EXIT_SUCCESS;
 }
