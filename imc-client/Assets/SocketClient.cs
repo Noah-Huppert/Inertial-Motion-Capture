6413ee46bf4eac6c22ab0d0a9dacde01188e1946
Noah Huppert <noahhuppert@gmail.com>
2015-09-04 00:33:27 -0400

    Added test client

---
+++ imc-client/Assets/SocketClient.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Net.Sockets;
+
+public delegate void ReadCallback(string result);
+
+public class SocketClient {
+	private TcpClient tcpClient;
+
+	private string host;
+	private int port;
+
+	private const int READ_BUFFER_SIZE = 256;
+	private byte[] readBuffer = new byte[READ_BUFFER_SIZE];
+
+	private ReadCallback clientReadCallback;
+
+	public SocketClient(string host, int port, ReadCallback clientReadCallback) {
+		this.host = host;
+		this.port = port;
+		this.clientReadCallback = clientReadCallback;
+	}
+
+	public void connect() {
+		tcpClient = new TcpClient (host, port);
+		Debug.Log ("Connected to socket " + host + ":" + port);
+	}
+
+	public void disconnect() {
+		if (tcpClient != null) {
+			tcpClient.Close();
+			Debug.Log ("Disconnected from socket " + host + ":" + port);
+		}
+	}
+
+	public void write(string sendContent) {
+		if (tcpClient.Connected) {
+			int sendBufferSize = sendContent.Length;
+			byte[] sendBuffer = new byte[sendBufferSize];
+			sendBuffer = Encoding.UTF8.GetBytes(sendContent);
+
+			tcpClient.GetStream().BeginWrite(sendBuffer, 0, sendBufferSize, new AsyncCallback(writeCallback), null);
+		} else {
+			Debug.Log("SocketClient must be connected");
+		}
+	}
+
+	public void read() {
+		if (tcpClient.Connected) {
+			tcpClient.GetStream().BeginRead(readBuffer, 0, READ_BUFFER_SIZE, new AsyncCallback(readCallback), null);
+		} else {
+			Debug.Log("SocketClient must be connected");
+		}
+	}
+
+	private void writeCallback(IAsyncResult asyncResult) {
+		tcpClient.GetStream ().EndWrite (asyncResult);
+	}
+
+	private void readCallback(IAsyncResult asyncResult) {
+		int bytesRead = tcpClient.GetStream ().EndRead (asyncResult);
+
+		if (bytesRead < 1) {
+			clientReadCallback("");
+			return;
+		}
+
+		string readResult = Encoding.ASCII.GetString (readBuffer, 0, bytesRead - 2);
+
+		clientReadCallback (readResult);
+	}
+}
---
+++ imc-client/Assets/SocketEchoController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SocketEchoController : MonoBehaviour {
+	private GameObject socketCube;
+	private SocketClient socketClient;
+
+	private float lastUpdateTime = 0;
+	private float updateTimeInterval = 0.001F;
+
+	private Vector3 socketCubePosition;
+
+	void Start() {
+		socketCube = GameObject.Find ("SocketCube");
+
+		socketClient = new SocketClient("192.168.1.9", 1234, socketReadCallback);
+
+		socketClient.connect ();
+	}
+
+	void Update() {
+		if (Math.Abs(Time.time - lastUpdateTime) >= updateTimeInterval) {
+			lastUpdateTime = Time.time;
+
+			Vector3 position = transform.position;
+			string positionString = position.x + " " + position.y + " " + position.z;
+			socketClient.write (positionString);
+			socketClient.read ();
+
+			socketCube.transform.position = socketCubePosition;
+		}
+	}
+
+	void onDestroy() {
+		socketClient.write ("EXIT");
+	}
+
+	void socketReadCallback(string readResult) {
+		if (readResult == "EXIT") {
+			socketClient.disconnect ();
+			return;
+		}
+
+		string[] posParts = readResult.Split (" "[0]);
+
+		socketCubePosition.x = float.Parse (posParts [0]);
+		socketCubePosition.y = float.Parse (posParts [1]) + 1.5F;
+		socketCubePosition.z = float.Parse (posParts [2]);
+	}
+}
