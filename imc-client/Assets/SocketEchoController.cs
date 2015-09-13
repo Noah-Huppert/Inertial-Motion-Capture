435556f1c51cf52ec8faba78747bc757557e6962
Noah Huppert <noahhuppert@gmail.com>
2015-09-12 22:10:59 -0400

    Updated client exit protocol, Updated client to interpret socket response

--- imc-client/Assets/SocketEchoController.cs
+++ imc-client/Assets/SocketEchoController.cs
@@ -7,17 +7,15 @@ using System.Net.Sockets;
 using System.Text;
 
 public class SocketEchoController : MonoBehaviour {
-	private GameObject socketCube;
 	private SocketClient socketClient;
 
 	private float lastUpdateTime = 0;
 	private float updateTimeInterval = 0.001F;
 
-	private Vector3 socketCubePosition;
+	private Vector3 position;
+	private Quaternion rotation;
 
 	void Start() {
-		socketCube = GameObject.Find ("SocketCube");
-
 		socketClient = new SocketClient("192.168.1.9", 1234, socketReadCallback);
 
 		socketClient.connect ();
@@ -27,29 +25,42 @@ public class SocketEchoController : MonoBehaviour {
 		if (Math.Abs(Time.time - lastUpdateTime) >= updateTimeInterval) {
 			lastUpdateTime = Time.time;
 
-			Vector3 position = transform.position;
-			string positionString = position.x + " " + position.y + " " + position.z;
-			socketClient.write (positionString);
+			socketClient.write ("NEXT");
 			socketClient.read ();
 
-			socketCube.transform.position = socketCubePosition;
+			transform.position = position;
+			transform.rotation  = rotation;
 		}
 	}
 
-	void onDestroy() {
+	public void exitButtonClicked() {
 		socketClient.write ("EXIT");
+		socketClient.disconnect ();
+		Application.Quit();
+		UnityEditor.EditorApplication.isPlaying = false;
 	}
 
 	void socketReadCallback(string readResult) {
-		if (readResult == "EXIT") {
-			socketClient.disconnect ();
-			return;
+		string[] stringParts = readResult.Split (" "[0]);
+		float[] parts = new float[stringParts.Length];
+
+		for (int i = 0; i < stringParts.Length; i++) {
+			parts[i] = float.Parse(stringParts[i]);
 		}
 
-		string[] posParts = readResult.Split (" "[0]);
+		if (parts.Length != 7) {
+			Debug.LogError ("Malformed server response \"" + readResult + "\"");
+		} else {
+			position.x = parts[0];
+			position.y = parts[1];
+			position.z = parts[2];
 
-		socketCubePosition.x = float.Parse (posParts [0]);
-		socketCubePosition.y = float.Parse (posParts [1]) + 1.5F;
-		socketCubePosition.z = float.Parse (posParts [2]);
+			rotation.w = parts[3];
+			rotation.x = parts[4];
+			rotation.y = parts[5];
+			rotation.z = parts[6];
+
+			Debug.Log ("position => " + position + " rotation => " + rotation);
+		}
 	}
 }
