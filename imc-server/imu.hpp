35bdb00fb4f992669c0dc2fcecb7a2a85801aa3a
Noah Huppert <noahhuppert@gmail.com>
2015-10-21 15:29:02 -0400

    Removed all position code from IMU class

--- imc-server/imu.hpp
+++ imc-server/imu.hpp
@@ -28,39 +28,21 @@ public:
     std::mutex rotation_lock;
     Quaternion rotation;
 
-    int df_size = 10;
-    DifferenceFilter accel_df_x = DifferenceFilter(df_size);
-    DifferenceFilter accel_df_y = DifferenceFilter(df_size);
-    DifferenceFilter accel_df_z = DifferenceFilter(df_size);
-
-    Vector3 last_accel;
-    Vector3 last_vel;
-    Vector3 vel_sum;
-
-    std::mutex position_lock;
-    Vector3 position;
-
-    IMU() {
-        //accel_df_y.difference_threshold = 0.3;
-        accel_df_z.difference_threshold = 0.25;
-    };
+    IMU() {};
     ~IMU() {};
 
     int start();
     int stop();
 
     int update_rotation();
-    int update_position();
-    int update();
 
 private:
-    long last_position_update_time = -1;
+    long last_rotation_update_time = -1;
 
     bool bno055_driver_bound = false;
     bool bno055_initialized = false;
     bool bno055_power_mode_normal = false;
     bool bno055_operation_mode_ndof = false;
-    bool bno055_accelerometer_sensitivity = false;
 
     /* BNO055 */
     struct bno055_t bno055;
